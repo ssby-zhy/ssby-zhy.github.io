df235fb0d772d1cc04db578734229fdfee9318d8
Zhang Hanyang <469548583@qq.com>
2022-07-06 15:36:44 +0800

    Site updated: 2022-07-06 15:36:44

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -2,12 +2,12 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         $('[rel="icon"]').attr('href', "/image/zhy.jpg");
          document.title = 'ヽ(●-`Д´-)ノ你丑你就走！';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         $('[rel="icon"]').attr('href', "/image/zhy.jpg");
          document.title = 'ヾ(Ő∀Ő3)ノ你帅就回来！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
